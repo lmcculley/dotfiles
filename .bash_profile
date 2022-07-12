d80f8fb8c0b9fc1bdfefc1f2de5c3e4f13762a32
Lance McCulley <lancemcculley@gmail.com>
2022-07-12 09:44:09 -0700

    Remove all old junk

--- .bash_profile
+++
@@ -1,4 +0,0 @@
-source $HOME/.bashrc
-
-test -e "${HOME}/.iterm2_shell_integration.bash" && source "${HOME}/.iterm2_shell_integration.bash"
-
--- configure.sh
+++
@@ -1,8 +0,0 @@
-cd ~
-mkdir -p ~/.vim/autoload ~/.vim/bundle; \
-    curl -Sso ~/.vim/autoload/pathogen.vim \
-    https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-git submodule update --init
-git submodule sync --recursive
-
-npm install -g eslint
